8272169d29d72e28868df55310650e90b2436a16
Kijpokin <kijpokin.nga@hotmail.com>
2021-10-14 21:10:46 +0700

    feat: add couter

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,25 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { Box } from '@mui/system'
+import { Button, Paper, Typography } from '@mui/material'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HM 1updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Box mt={2} pt={5}>
+        <Button variant="contained" onClick={() => setCount(count => count + 1)}>Click me</Button>
+        <Box component="div" mt={2}  p={5}>
+        <Paper >
+          <Box p={5}>
+          <Typography>Counter: {count}</Typography>
+          </Box>
+        </Paper>
+        </Box>
+      
+      </Box>
     </div>
   )
 }
